5009ea57db3e98e10545217d6e9a8eb9258b20eb
Kordzakhiaa <giorgi.kordzakhia.2@btu.edu.ge>
2021-11-18 20:56:56 +0400

    Modified: main.py added new paths for creating blog and getting all blogs

--- blog/main.py
+++ blog/main.py
@@ -1,5 +1,21 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, Depends
+from sqlalchemy.orm import Session
+
+import models
+import schemas
+from database import engine, SessionLocal
 from schemas import Blog
+
+models.Blog.metadata.create_all(engine)  # Creating all tables
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 app = FastAPI()
 
@@ -10,5 +26,17 @@ def index():
 
 
 @app.post('/create', status_code=status.HTTP_201_CREATED)
-def create(request: Blog):
-    return {'data': request, 'status': 201}
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+
+    return {'data': {'new_blog': new_blog}}
+
+
+@app.get('/all_blogs', status_code=status.HTTP_200_OK)
+def blogs(db: Session = Depends(get_db)):
+    all_blogs = db.query(models.Blog).all()
+
+    return {'all_blogs': all_blogs}
