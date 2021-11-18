4dc141929dc9b2057cf87f4f38addcea29223c6d
Kordzakhiaa <giorgi.kordzakhia.2@btu.edu.ge>
2021-11-18 21:33:45 +0400

    Added: delete blog endpoint

--- blog/main.py
+++ blog/main.py
@@ -1,15 +1,15 @@
-from fastapi import FastAPI, status, Depends
+from fastapi import FastAPI, Depends, status, HTTPException, Response
 from sqlalchemy.orm import Session
 
 import models
-import schemas
 from database import engine, SessionLocal
-from schemas import Blog
+import schemas
 
 models.Blog.metadata.create_all(engine)  # Creating all tables
 
 
 def get_db():
+    """ Function that gets database """
     db = SessionLocal()
     try:
         yield db
@@ -22,11 +22,15 @@ app = FastAPI()
 
 @app.get('/', status_code=status.HTTP_200_OK)
 def index():
-    return {'message': 'index page', 'status': 200}
+    """ Index page """
+
+    return {'message': 'index page'}
 
 
 @app.post('/create', status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    """ View that creates blog """
+
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
@@ -37,6 +41,31 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
 
 @app.get('/all_blogs', status_code=status.HTTP_200_OK)
 def blogs(db: Session = Depends(get_db)):
-    all_blogs = db.query(models.Blog).all()
+    """ View that lists all blogs """
 
+    all_blogs = db.query(models.Blog).all()
     return {'all_blogs': all_blogs}
+
+
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+def get_blog(id: int, db: Session = Depends(get_db)):
+    """ View where you can get specific blog by id """
+
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Blog with the id {id} not found')
+    return {'blog': blog}
+
+
+@app.delete('/blog/delete/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_blog(id: int, db: Session = Depends(get_db)):
+    """ View which ensures the deletion of a specific blog """
+
+    blog = db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    if blog < 1:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Blog with the id {id} not found and cannot be deleted')
+    db.commit()
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
